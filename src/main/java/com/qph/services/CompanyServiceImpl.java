944fa3ebf94eddfc39a0b09bb9a5338559f7526e
Quoc PHAN <phquoc25@gmail.com>
2018-10-28 00:05:52 +0700

    [t09-resource-server] Secure methods access with @PreAuthorize

--- src/main/java/com/qph/services/CompanyServiceImpl.java
+++ src/main/java/com/qph/services/CompanyServiceImpl.java
@@ -2,7 +2,9 @@ package com.qph.services;
 
 import com.qph.beans.Company;
 import com.qph.repository.CompanyRepository;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -16,31 +18,43 @@ public class CompanyServiceImpl implements CompanyService {
     }
 
     @Override
+    @Transactional(readOnly = true)
+    @PreAuthorize("hasRole('COMPANY_READER')")
     public List<Company> getAll() {
         return (List<Company>) companyRepository.findAll();
     }
 
     @Override
+    @Transactional(readOnly = true)
+    @PreAuthorize("hasAuthority('COMPANY_READ') and hasAuthority('DEPARTMENT_READ')")
     public Optional<Company> get(Long id) {
         return companyRepository.findById(id);
     }
 
     @Override
+    @Transactional(readOnly = true)
+    @PreAuthorize("hasAuthority('COMPANY_READ') and hasAuthority('DEPARTMENT_READ')")
     public Optional<Company> get(String name) {
         return companyRepository.findByName(name);
     }
 
     @Override
+    @Transactional
+    @PreAuthorize("hasAuthority('COMPANY_CREATE')")
     public Company create(Company company) {
         return companyRepository.save(company);
     }
 
     @Override
+    @Transactional
+    @PreAuthorize("hasAuthority('COMPANY_UPDATE')")
     public Company update(Company company) {
         return companyRepository.save(company);
     }
 
     @Override
+    @Transactional
+    @PreAuthorize("hasAuthority('COMPANY_DELETE')")
     public void delete(Long id) {
         companyRepository.deleteById(id);
     }
