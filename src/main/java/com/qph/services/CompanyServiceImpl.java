fd64cc3c44780b53850cc61ea3fe89f6af7a86b8
Quoc PHAN <phquoc25@gmail.com>
2019-01-21 23:21:30 +0700

    [t10-basic-auth] update methods security by using @PreAuthorize

--- src/main/java/com/qph/services/CompanyServiceImpl.java
+++ src/main/java/com/qph/services/CompanyServiceImpl.java
@@ -2,6 +2,7 @@ package com.qph.services;
 
 import com.qph.beans.Company;
 import com.qph.repository.CompanyRepository;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -16,31 +17,37 @@ public class CompanyServiceImpl implements CompanyService {
     }
 
     @Override
+    @PreAuthorize("hasAuthority('COMPANY_READ')")
     public List<Company> getAll() {
         return (List<Company>) companyRepository.findAll();
     }
 
     @Override
+    @PreAuthorize("hasAuthority('COMPANY_READ')")
     public Optional<Company> get(Long id) {
         return companyRepository.findById(id);
     }
 
     @Override
+    @PreAuthorize("hasAuthority('COMPANY_READ')")
     public Optional<Company> get(String name) {
         return companyRepository.findByName(name);
     }
 
     @Override
+    @PreAuthorize("hasAuthority('COMPANY_CREATE')")
     public Company create(Company company) {
         return companyRepository.save(company);
     }
 
     @Override
+    @PreAuthorize("hasAuthority('COMPANY_UPDATE')")
     public Company update(Company company) {
         return companyRepository.save(company);
     }
 
     @Override
+    @PreAuthorize("hasAuthority('COMPANY_DELETE')")
     public void delete(Long id) {
         companyRepository.deleteById(id);
     }
